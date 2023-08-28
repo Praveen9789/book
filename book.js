366aacbe7c302f583825350c7f848c197e472927
Praveen9789 <131514392+Praveen9789@users.noreply.github.com>
2023-08-28 11:47:48 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,136 @@
+let book1 = {
+    title: "No-Drama Discipline",
+    author: "Daniel J. Siegel",
+    yearpublished: 2014,
+    summary: function () {
+        document.getElementById("demo1").innerHTML="NEW YORK TIMES BESTSELLER • The pioneering experts behind The Whole-Brain Child and The Yes Brain tackle the ultimate parenting challenge";
+    },
+    add: function () {
+        document.getElementById("add1").innerHTML = "Added";
+    }
+};
+let book2 = {
+    title: "How to Talk",
+    author: "Adele Faber",
+    yearpublished: 2001,
+    summary: function () {
+        document.getElementById("demo2").innerHTML="The number one practical guide to making your family life less stressful and more rewarding.In this international bestseller, experts Adele Faber & Elaine Mazlishprovide effective step by step techniques "
+    }
+};
+let book3 = {
+    title: "The whole Brain Child",
+    author: "Adele Faber",
+    yearpublished: 1980,
+    summary: function () {
+        document.getElementById("demo3").innerHTML="In this pioneering, practical book for parents, neuroscientist Daniel J. Siegel and parenting expert Tina Payne Bryson explain the new science of how a child's brain is wired and how it matures"
+    }
+};
+let book4 = {
+    title: "Raising Good Humans",
+    author: "Hunter Clarke-Fields",
+    yearpublished: 2019,
+    summary: function () {
+        document.getElementById("demo4").innerHTML = "A wise and fresh approach to mindful parenting —Tara Brach, author of Radical Acceptance A kinder, more compassionate world starts with kind and compassionate kids"
+    }
+};
+let book5 = {
+    title: "The Explosive Child",
+    author: "Ross W. Greenenvv",
+    yearpublished: 1998,
+    summary: function () {
+        document.getElementById("demo5").innerHTML="A noted therapist offers a groundbreaking approach to understanding and parenting temperamental children, reducing tension and improving family relationships for good. A tremendous resource and a must-read for adults involved with easily frustrated children.--Bloomsbury Review Photos."
+    }
+};
+let book6 = {
+    title: "Siblings Without Rivalry",
+    author: "Adele Faber",
+    yearpublished: 1987,
+    summary: function () {
+        document.getElementById("demo6").innerHTML="The #1 New York Times best-selling guide to reducing hostility and generating goodwill between siblings. Already best-selling authors with How to Talk So Kids Will Listen & Listen So Kids Will Talk, Adele Faber and Elaine Mazlish turned their minds to the battle of the siblings. "
+    }
+};
+let book7 = {
+    title: "The Book Thief",
+    author: "Markus Zusak",
+    yearpublished: 2005,
+    summary: function () {
+        document.getElementById("demo7").innerHTML="The Book Thief is a historical fiction novel by the Australian author Markus Zusak, set in Nazi Germany during World War II. Published in 2006, The Book Thief became an international bestseller and was translated into 63 languages and sold 17 million copies. It was adapted into the 2013 feature film, The Book Thief."
+    }
+};
+let book8 = {
+    title: "The Fault in Our Stars",
+    author: "John Green",
+    yearpublished: 2012,
+    summary: function () {
+        document.getElementById("demo8").innerHTML="The Fault in Our Stars is a novel by John Green. It is his fourth solo novel, and sixth novel overall. It was published on January 10, 2012."
+    }
+};
+let book9 = {
+    title: "Speak",
+    author: "Laurie Halse Anderson",
+    yearpublished: 1999,
+    summary: function () {
+        document.getElementById("demo9").innerHTML="Lord of the Flies is a 1954 novel by the Nobel laureate British author William Golding. The plot concerns a group of British boys who are stranded on an uninhabited island and their disastrous attempts to govern themselves."
+    }
+};
+let book10 = {
+    title: "The Catcher in the Rye",
+    author: "J. D. Salinger",
+    yearpublished: 1951,
+    summary: function () {
+        document.getElementById("demo10").innerHTML="The Catcher in the Rye is an American novel by J. D. Salinger that was partially published in serial form 1945–46 before being novelized in 1951. Originally intended for adults, it is often read by adolescents for its themes of angst and alienation, and as a critique of superficiality in society."
+    }
+};
+let book11 = {
+    title: "Lord of the Flies",
+    author: "William Golding",
+    yearpublished: 1954,
+    summary: function () {
+        document.getElementById("demo11").innerHTML="Lord of the Flies is a 1954 novel by the Nobel laureate British author William Golding. The plot concerns a group of British boys who are stranded on an uninhabited island and their disastrous attempts to govern themselves."
+    }
+};
+let book12 = {
+    title: "The Fault in Our Stars",
+    author: "John Green",
+    yearpublished: 2012,
+    summary: function () {
+        document.getElementById("demo12").innerHTML = "Hazel and Augustus are two teenagers who share an acerbic wit, a disdain for the conventional, and a love that sweeps them on a journey."
+    }
+}; 
+
+function addd1() {
+    alert(book1.title + " book Added");
+}
+function addd2() {
+    alert(book2.title + " book Added");
+}
+function addd3() {
+    alert(book3.title + " book Added");
+}
+function addd4() {
+    alert(book4.title + " book Added");
+}
+function addd5() {
+    alert(book5.title + " book Added");
+}
+function addd6() {
+    alert(book6.title + " book Added");
+}
+function addd7() {
+    alert(book7.title + " book Added");
+}
+function addd8() {
+    alert(book8.title + " book Added");
+}
+function addd9() {
+    alert(book9.title + " book Added");
+}
+function addd10() {
+    alert(book10.title + " book Added");
+}
+function addd11() {
+    alert(book11.title + " book Added");
+}
+function addd12() {
+    alert(book12.title + " book Added");
+}
